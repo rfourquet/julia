dbfc3e674ea67deb6c9d2e418636443216b52705
Matt Bauman <mbauman@gmail.com>
2014-05-04 20:00:24 -0400

    Speed up accesses by ~2x by specializing on the iterble types

--- base/generator.jl
+++ base/generator.jl
@@ -1,20 +1,40 @@
 # Remove once part of standard library:
 import Base: return_type, show, writemime, size, strides, isassigned, getindex, convert, ntuple
 
-immutable Generator{T,N} <: DenseArray{T,N}
+abstract AbstractGenerator{T,N} <: DenseArray{T,N}
+
+immutable Generator1{T,I} <: AbstractGenerator{T,1}
     name::UTF8String
     func::Function # must return elements of type T
-    itrs::NTuple{N}
-    
-    Generator(func::Function, itrs::NTuple{N}) = new("?? for ?? in ??s", func, itrs)
-    Generator(name::String, func::Function, itrs::NTuple{N}) = new(name, func, itrs)
+    itr1::I
+end
+immutable Generator2{T,I1,I2} <: AbstractGenerator{T,2}
+    name::UTF8String
+    func::Function
+    itr1::I1
+    itr2::I2
+end
+immutable NGenerator{T,N,IS} <: AbstractGenerator{T,N}
+    name::UTF8String
+    func::Function
+    itrs::IS
 end
 
-Generator(name::String, func::Function, itrs...) = Generator(name, func, itrs)
-function Generator{N}(name::String, func::Function, itrs::NTuple{N})
-    arg_types = map(eltype, itrs)::Type
-    T = return_type(func, arg_types)
-    Generator{T,N}(name, func, itrs)
+Generator(name, func, itr1)       = Generator1(name, func, itr1)
+Generator(name, func, itr1, itr2) = Generator2(name, func, itr1, itr2)
+Generator(name, func, itrs...)    = NGenerator(name, func, itrs)
+
+function Generator1{I}(name::String, func::Function, itr::I)
+    T = return_type(func, (eltype(itr),))
+    Generator1{T,I}(name, func, itr)
+end
+function Generator2{I1,I2}(name::String, func::Function, itr1::I1, itr2::I2)
+    T = return_type(func, (eltype(itr1), eltype(itr2)))
+    Generator2{T,I1,I2}(name, func, itr1, itr2)
+end
+function NGenerator{I}(name::String, func::Function, itrs::I)
+    T = return_type(func, map(eltype, itrs)::Type)
+    NGenerator{T,length(I),I}(name, func, itrs)
 end
 
 macro generator(thunk, assignments...)
@@ -28,42 +48,53 @@ macro generator(thunk, assignments...)
 end
 
 # Display without showing the contents
-writemime(io::IO, ::MIME"text/plain", g::Generator) = show(io, g)
-show(io::IO, g::Generator) = print(io, summary(g), ": ", g.name)
+writemime(io::IO, ::MIME"text/plain", g::AbstractGenerator) = show(io, g)
+show(io::IO, g::AbstractGenerator) = print(io, summary(g), ": ", g.name)
 
 ## Basic functions ##
-size(g::Generator) = map(length, g.itrs)
-size{T,N}(g::Generator{T,N}, d) = d > N ? 1 : length(g.itrs[d])
-strides{T}(g::Generator{T,1}) = (1,)
-strides{T}(g::Generator{T,2}) = (1, size(g,1))
-strides{T}(g::Generator{T,3}) = (1, size(g,1), size(g,1)*size(g,2))
-isassigned(g::Generator, i::Int) = 1 <= i <= length(g)
+size(g::NGenerator) = map(length, g.itrs)
+size{T,N}(g::NGenerator{T,N}, d) = d > N ? 1 : length(g.itrs[d])
+size(g::Generator1)    = (length(g.itr1),)
+size(g::Generator1, d) = d > 1 ? 1 : length(g.itrs[d])
+size(g::Generator2)    = (length(g.itr1), length(g.itr2))
+size(g::Generator2, d) = d == 1 ? length(g.itr1) : 
+                         d == 2 ? length(g.itr2) : 1
+
+strides{T}(g::AbstractGenerator{T,1}) = (1,)
+strides{T}(g::AbstractGenerator{T,2}) = (1, size(g,1))
+strides{T}(g::AbstractGenerator{T,3}) = (1, size(g,1), size(g,1)*size(g,2))
+isassigned(g::AbstractGenerator, i::Int) = 1 <= i <= length(g)
 
 ## Conversion and expansion ##
-full{T,N}(g::Generator{T,N}) = convert(Array{T,N}, g)
-convert{T,S,N}(::Type{Array{T}}, g::Generator{S,N}) = convert(Array{T,N},g)
-function convert{T,S,N}(::Type{Array{T,N}}, g::Generator{S,N})
+function full{T,N}(g::AbstractGenerator{T,N})
     A = Array(T, size(g))
-    for i = 1:length(A)
+    @inbounds for i = 1:length(A)
         A[i] = g[i]
     end
     return A
 end
+convert{T,S,N}(::Type{Array{T}}, g::AbstractGenerator{S,N}) = full(g)
 
-# Generic case (with linear indexing)
-function getindex{T,N}(g::Generator{T,N}, Idxs::Real...)
+# Indexing: obvious easy cases
+getindex(g::Generator1, i1::Real)           = g.func(g.itr1[i1])
+getindex(g::Generator2, i1::Real, i2::Real) = g.func(g.itr1[i1], g.itr2[i2])
+function getindex(g::Generator2, i1::Real)
+    subs = ind2sub(size(g), i1)
+    g.func(g.itr1[subs[1]], g.itr2[subs[2]])
+end
+# Generic cases (with linear indexing)
+function getindex{T,N}(g::NGenerator{T,N}, Idxs::Real...)
     length(Idxs) == N && return g.func(map(getindex, g.itrs, Idxs)...)
-    # The intermediate case 1 < length(Idxs) < N could be handled better
+    # The intermediate case 1<length(Idxs)<N could be handled more efficiently
     linear_idx = length(Idxs) > 1 ? sub2ind(size(g),Idxs...) : Idxs[1]
     subs = ind2sub(size(g), linear_idx)
     g.func(map(getindex, g.itrs, subs)...)
 end
-# Indexing: obvious easy cases
-getindex{T}(g::Generator{T,1}, i1::Real)                               = g.func(g.itrs[1][i1])
-getindex{T}(g::Generator{T,2}, i1::Real, i2::Real)                     = g.func(g.itrs[1][i1], g.itrs[2][i2])
-getindex{T}(g::Generator{T,3}, i1::Real, i2::Real, i3::Real)           = g.func(g.itrs[1][i1], g.itrs[2][i2], g.itrs[3][i3])
-getindex{T}(g::Generator{T,4}, i1::Real, i2::Real, i3::Real, i4::Real) = g.func(g.itrs[1][i1], g.itrs[2][i2], g.itrs[3][i3], g.itrs[4][i4])
+getindex{T}(g::NGenerator{T,1}, i1::Real)                               = g.func(g.itrs[1][i1])
+getindex{T}(g::NGenerator{T,2}, i1::Real, i2::Real)                     = g.func(g.itrs[1][i1], g.itrs[2][i2])
+getindex{T}(g::NGenerator{T,3}, i1::Real, i2::Real, i3::Real)           = g.func(g.itrs[1][i1], g.itrs[2][i2], g.itrs[3][i3])
+getindex{T}(g::NGenerator{T,4}, i1::Real, i2::Real, i3::Real, i4::Real) = g.func(g.itrs[1][i1], g.itrs[2][i2], g.itrs[3][i3], g.itrs[4][i4])
 
 # Some potential convenient uses of generators
-ntuple(g::Generator) = ntuple(length(g), i -> g[i])
-# Dict(g::Generator) just works as inherited from AbstractArray
+ntuple(g::AbstractGenerator) = ntuple(length(g), i -> g[i])
+# Dict(g::AbstractGenerator) just works as inherited from AbstractArray
